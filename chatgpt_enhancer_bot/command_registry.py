38a85348d49b1d7d5718e914125b508272fd1d06
Petr Lavrov <calmquant@gmail.com>
2023-01-20 01:36:53 +0300

    rework_markdown
    Disable all markdown except code blocks for now :(

--- chatgpt_enhancer_bot/command_registry.py
+++ chatgpt_enhancer_bot/command_registry.py
@@ -11,9 +11,10 @@ class CommandRegistry:
         self.descriptions = {}
         self.docstrings = {}
         self.groups = {}
-        self._is_markdown_safe = {}
+        # self._is_markdown_safe = {}
 
-    def register(self, shortcuts=None, group=None, is_markdown_safe=False):
+    # def register(self, shortcuts=None, group=None, is_markdown_safe=False):
+    def register(self, shortcuts=None, group=None):
         """
         Register a function as a command
         :param shortcuts: list of shortcuts for a function
@@ -30,14 +31,16 @@ class CommandRegistry:
             doc = func.__doc__
             if not shortcuts:
                 shortcuts.append(f"/{name}")
-            self.add_command(name, shortcuts, doc, group or func.__class__, is_markdown_safe=is_markdown_safe)
+            # self.add_command(name, shortcuts, doc, group or func.__class__, is_markdown_safe=is_markdown_safe)
+            self.add_command(name, shortcuts, doc, group or func.__class__)
 
             func.__shortcuts__ = shortcuts
             return func
 
         return wrapper
 
-    def add_command(self, command, shortcuts, docstring, group, is_markdown_safe=False):
+    # def add_command(self, command, shortcuts, docstring, group, is_markdown_safe=False):
+    def add_command(self, command, shortcuts, docstring, group):
         desc = docstring.strip().splitlines()[
             0] if docstring else "This docstring is missing!! Abuse @petr_lavrov until he writes it!!"
 
@@ -46,7 +49,7 @@ class CommandRegistry:
             self.descriptions[shortcut] = desc
             self.docstrings[shortcut] = docstring
             self.groups[shortcut] = group
-            self._is_markdown_safe[shortcut] = is_markdown_safe
+            # self._is_markdown_safe[shortcut] = is_markdown_safe
 
         # todo: add command as a separate object as well - avoid duplication in help command. Dataclass?
 
@@ -73,5 +76,5 @@ class CommandRegistry:
     def get_group(self, command):
         return self.groups[command]
 
-    def is_markdown_safe(self, command):
-        return self._is_markdown_safe[command]
+    # def is_markdown_safe(self, command):
+    #     return self._is_markdown_safe[command]
